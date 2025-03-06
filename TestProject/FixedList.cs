0916ca72c477c4fa419ce3a94022273f61265fc6
Grigori Hovhannisyan <ghovhannisyan213@gmail.com>
2025-03-06 14:03:51 +0400

    First Commit

---
+++ TestProject/FixedList.cs
@@ -0,0 +1,143 @@
+﻿using System.Collections;
+
+namespace TestProject
+{
+    public class FixedList<T> : IEnumerable
+    {
+        public FixedList(int lenght)
+        {
+            values = new T[lenght];
+        }
+        T[] values;
+        int _freeSpaceFromEnd = 0;
+        int? _freeSpace;
+        bool _skipNullOrDefault = false;
+        public T this[int index]
+        {
+            get
+            {
+                return values[index];
+            }
+            set
+            {
+                values[index] = value;
+            }
+        }
+
+        public FixedList<T> SkipNullOrDefault(bool val)
+        {
+            this._skipNullOrDefault = val;
+
+            return this;
+        }
+
+        public T Add(T value)
+        {
+            int index = _freeSpace ?? _freeSpaceFromEnd;
+            bool incrise = false;
+            if (_freeSpace == null)
+            {
+                incrise = true;
+            }
+            values[index] = value;
+            if (incrise)
+            {
+                _freeSpaceFromEnd++;
+            }
+            _freeSpace = null;
+            return value; ;
+        }
+
+        public bool Remove(T value)
+        {
+            int index = FindIndex(value);
+            if (index >= 0)
+            {
+                values[index] = default(T);
+                _freeSpace = index;
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        public int FindIndex(T obj)
+        {
+            if (obj == null)
+                throw new NullReferenceException();
+
+            int pos = 0;
+            do
+            {
+                if (pos < values.Length)
+                {
+                    EqualityComparer<T> equalityComparer = EqualityComparer<T>.Default;
+                    if (equalityComparer.Equals(obj, values[pos]))
+                    {
+                        return pos;
+                    }
+                    pos++;
+                }
+                else
+                {
+                    return -1;
+                }
+            } while (true);
+        }
+
+        public IEnumerator GetEnumerator()
+        {
+            return new Enumerator(this, _skipNullOrDefault);
+        }
+
+        public struct Enumerator : IEnumerator<T>
+        {
+            int _index = -1;
+            FixedList<T> _list;
+            bool _skipNullOrDefault;
+            public Enumerator(FixedList<T> list)
+            {
+                _list = list;
+            }
+
+            public Enumerator(FixedList<T> list, bool skipNullOrDefault) : this(list)
+            {
+                _skipNullOrDefault = skipNullOrDefault;
+            }
+
+            public T Current => _list[_index];
+
+            object IEnumerator.Current => Current;
+
+            public void Dispose()
+            {
+            }
+
+            public bool MoveNext()
+            {
+                _index++;
+                T? defaultValue = default(T);
+                if (_list.values == null || _index >= _list.values.Length)
+                {
+                    return false;
+                }
+                else if (defaultValue == null || defaultValue.Equals(_list.values[_index]))
+                {
+                    if (_skipNullOrDefault)
+                        return MoveNext();
+                    return true;
+                }
+
+                return true;
+            }
+
+
+            public void Reset()
+            {
+                _index = -1;
+            }
+        }
+    }
+}
---
+++ TestProject/Program.cs
@@ -0,0 +1,34 @@
+﻿using TestProject;
+
+FixedList<Guid> a = new FixedList<Guid>(10);
+
+Guid test = Guid.NewGuid();
+
+a.Add(Guid.NewGuid());
+a.Add(Guid.NewGuid());
+a.Add(Guid.NewGuid());
+a.Add(Guid.NewGuid());
+a.Add(test);
+a.Add(Guid.NewGuid());
+a.Add(Guid.NewGuid());
+a.Add(Guid.NewGuid());
+a.Add(Guid.NewGuid());
+
+foreach (var i in a) { Console.WriteLine(i); }
+
+Console.ReadLine();
+
+a.Remove(test);
+foreach (var i in a) { Console.WriteLine(i); }
+
+Console.ReadLine();
+
+a.Add(test);
+foreach (var x in a) { Console.WriteLine(x); }
+
+Console.ReadLine();
+
+a.Add(Guid.NewGuid());
+foreach (var x in a) { Console.WriteLine(x); }
+
+Console.ReadLine();
